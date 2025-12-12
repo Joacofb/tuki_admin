5a66077352e350cf4edc674abd9a88a2c71d4cb6
Joacofb <joaquinbrischetto@gmail.com>
2025-12-12 17:01:33 +0100

    Fix imports and add links

--- apps/vehiculos/models.py
+++ apps/vehiculos/models.py
@@ -37,7 +37,7 @@ class VehicleModel(models.Model):
     def __str__(self):
         return f'{self.vehiclemodel_brand} - {self.vehiclemodel_name} - {self.vehiclemodel_version}'
 
-from customer.models import Customer
+# from customer.models import Customer
 
 class Vehicle(models.Model):
     vehicle_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
