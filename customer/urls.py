9472abb70d4d92bb17985787ae1485db9d5f1ebd
Joacofb <joaquinbrischetto@gmail.com>
2025-12-13 13:11:02 +0100

    Refactor customer: list, detail, add and delete w/template. Views and urls working

--- customer/urls.py
+++ customer/urls.py
@@ -7,4 +7,6 @@ app_name = "customer"
 urlpatterns = [
     path("", views.customer_list, name="list"),
     path("<uuid:customer_id>/", views.customer_detail, name="detail"),
+    path("add/", views.add_customer, name="add"),
+    path("<uuid:customer_id>/delete", views.delete_customer, name="delete"),
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,11 +1,23 @@
 # customer/views.py
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
+from django.contrib import messages
 from .models import Customer
 
 
 def customer_list(request):
+    search_data = request.GET.get('search_customer', '')
+
     customers = Customer.objects.all().order_by("customer_name")
-    return render(request, "customer/customer_list.html", {"customers": customers})
+
+    if search_data:
+        customers = customers.filter(
+            Q(customer_name__icontains=search_data)
+            | Q(customer_email__icontains=search_data)
+            | Q(customer_phone__icontains=search_data)
+        )
+
+    return render(request, "customer/all_customers.html", {"customers": customers})
 
 
 def customer_detail(request, customer_id):
@@ -25,3 +37,42 @@ def customer_detail(request, customer_id):
         "vehicle_workorders": vehicle_workorders,
     }
     return render(request, "customer/customer_detail.html", context)
+
+def add_customer(request):
+
+    if request.method == "POST":
+        customer_name = request.POST.get("customer_name", "").strip()
+        customer_phone = request.POST.get("customer_phone", "").strip()
+        customer_email = request.POST.get("customer_email", "").strip()
+
+        customer_type = request.POST.get("customer_type", "").strip()
+        customer_billing = request.POST.get("customer_billing", "").strip()
+
+        if customer_name and customer_phone:
+            customer = Customer.objects.create(
+                customer_name=customer_name,
+                customer_phone=customer_phone,
+                customer_email=customer_email or None,
+                customer_type=customer_type or None,
+                customer_billing=customer_billing or None,
+            )
+            
+            return redirect('customer:detail', customer_id=customer.customer_id)
+
+        else:
+            messages.error(request, "Asigna un nombre y un telefono para crear un cliente.")
+
+
+    return render(request, 'customer/add_customer.html')
+
+def delete_customer(request, customer_id):
+    get_customer = get_object_or_404(Customer, customer_id=customer_id)
+    context = {
+        'customer': get_customer,
+    }
+
+    if request.method == 'POST':
+        get_customer.delete()
+        return redirect('customer:list')
+    
+    return render(request, "customer/delete_customer.html", context)
